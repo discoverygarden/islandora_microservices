46e8d0dff7cfb93010a7c1a1497c0899a5cf49b3
Adam Vessey <adam.vessey@gmail.com>
2012-01-05 10:20:20 -0500

    Fixes for IR

--- plugins/fjm/__init__.py
+++ plugins/fjm/__init__.py
@@ -1,7 +1,12 @@
 
 from plugin_manager import IslandoraListenerPlugin as ILP
-from islandoraUtils import DSConverter as CONV
+from islandoraUtils import DSConverter as CONV, fedoraLib as FL
 import logging
+from os import path
+from shutil import rmtree
+import mimetypes
+from islandoraUtils.xmlib import import_etree
+etree = import_etree()
 
 class fjm(ILP):
     def __init__(self):
@@ -18,9 +23,86 @@ class fjm(ILP):
                 CONV.create_thumbnail(obj, 'JPG', 'TN')
             elif message['dsid'] == 'MARCXML':
                 CONV.marcxml_to_mods(obj, 'MARCXML', 'MODS')
-            elif message['dsid'] == 'WAV':
-                CONV.create_mp3(obj, 'WAV', 'MP3')
-            
+            elif message['dsid'] == 'ENDNOTE' and 'ENDNOTE' in obj and 'MODS' in obj:
+                self.__fix_ir_ingest(obj)
+        elif message['method'] == 'ingest':
+            if 'ENDNOTE' in obj and 'MODS' in obj: #FIXME:  This might be better off implemented on the PHP side, via the Bibutils post-process hook... (Which I did not know existed for the initial implementation...  Or it might be better off done here...  Dunno...)
+                self.__fix_ir_ingest(obj)
 
     def islandoraMessage(self, method, message, client):
         pass
+        
+    def __fix_ir_ingest(self, obj):
+        logger = self.logger
+        ir_ingest_dir = '/mnt/islandora/ToIngest/ceacs/citation'  #Hack-tastic...
+        endnote = etree.fromstring(obj['ENDNOTE'].getContent().read())
+        mods = etree.fromstring(obj['MODS'].getContent().read())
+        DSs = dict() #FIXME:  Should probably load the entire list...
+        namespaces = {
+          'mods': 'http://www.loc.gov/mods/v3'
+        }
+        removing_url = False
+        for related_url in endnote.findall('records/record/urls/related-urls/url/style'):
+            #If the url doesn't seem to contain a scheme specifier, assume it's relative to the 'ir_ingest_dir'
+            related_text = related_url.text
+            if '://' not in related_text:
+                logger.debug('Found apparent relative %s' % related_url.text)
+                dsid = related_text.rpartition('.')[2].strip().upper()
+                logging
+                obj._dsids = None
+                if dsid not in obj:
+                    DSs[dsid] = 1
+                else:
+                    dsid = dsid + DSs[dsid]
+                    DSs[dsid] += 1
+                FL.update_hashed_datastream_without_dup(obj, dsid, path.join(ir_ingest_dir, related_text), mimeType="application/pdf", checksumType='SHA-1')
+                
+                #Get rid of the URL...
+                style = related_url
+                url = style.getparent()
+                related_urls = url.getparent()
+                related_urls.remove(url)
+                urls = related_urls.getparent()
+                if len(related_urls) == 0 and len(urls) == 1:
+                    urls.getparent().remove(urls)
+                
+                for url in mods.findall('{%(mods)s}location/{%(mods)s}url' % namespaces):
+                    if url.text == related_text:
+                        parent = url.getparent()
+                        parent.remove(url)
+                        if len(parent) == 0:
+                            parent.getparent().remove(parent)
+                        removing_url = True
+                        break
+                        
+        found = False
+        lastGenre = None
+        for genre in mods.findall('{%(mods)s}genre' % namespaces):
+          type = genre.get('authority')
+          lastGenre = genre
+          if type == 'endnote':
+            found = True
+            break
+
+        if not found:
+            rt = endnote.find('records/record/ref-type')
+            
+            #XXX: Hack to deal with type not recognized by Bibutils... (Assuming that if already in MODS, it is correct...)
+            if rt.get('name') == 'Book':
+                wt = endnote.find('records/record/work-type')
+                if wt.get('name') == 'Working Paper':
+                    rt = wt
+                    
+            reftype = rt.get('name')
+            #4.1.2 and add it to the MODS in the genre (just as child of /mods:mods)
+            if reftype is not None:
+              reftype_el = etree.Element('genre')
+              reftype_el.text = reftype
+              reftype_el.set('authority', 'endnote')
+              if lastGenre != None:
+                mods.insert(mods.index(lastGenre), reftype_el)
+              else:
+                mods.append(reftype_el)
+            
+        if removing_url or not found:
+            obj['MODS'].setContent(etree.tounicode(mods, pretty_print=True))
